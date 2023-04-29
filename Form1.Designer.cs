f40e020ce002517b9877e25231c96751af9f3c9a
modeste TSIBA <tsibacaleb@gmail.com>
2023-04-30 00:15:17 +0200

    modif visuelle

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,22 +38,23 @@
             this.black = new System.Windows.Forms.Button();
             this.white = new System.Windows.Forms.Button();
             this.lookForColor = new System.Windows.Forms.Button();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.scrollertaille)).BeginInit();
-            this.flowLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.rouge);
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.flowLayoutPanel1.Controls.Add(this.vert);
-            this.flowLayoutPanel1.Controls.Add(this.jaune);
-            this.flowLayoutPanel1.Controls.Add(this.bleur);
             this.flowLayoutPanel1.Controls.Add(this.gris);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Controls.Add(this.bleur);
+            this.flowLayoutPanel1.Controls.Add(this.rouge);
+            this.flowLayoutPanel1.Controls.Add(this.jaune);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 6);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(425, 28);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(161, 71);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // rouge
@@ -62,11 +63,10 @@
             this.rouge.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.rouge.Font = new System.Drawing.Font("Marlett", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rouge.ForeColor = System.Drawing.SystemColors.Control;
-            this.rouge.Location = new System.Drawing.Point(3, 3);
+            this.rouge.Location = new System.Drawing.Point(96, 3);
             this.rouge.Name = "rouge";
-            this.rouge.Size = new System.Drawing.Size(75, 23);
+            this.rouge.Size = new System.Drawing.Size(25, 25);
             this.rouge.TabIndex = 0;
-            this.rouge.Text = "rouge";
             this.rouge.UseVisualStyleBackColor = false;
             this.rouge.MouseDown += new System.Windows.Forms.MouseEventHandler(this.rouge_MouseDown);
             // 
@@ -76,11 +76,10 @@
             this.vert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.vert.Font = new System.Drawing.Font("Marlett", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.vert.ForeColor = System.Drawing.SystemColors.Control;
-            this.vert.Location = new System.Drawing.Point(84, 3);
+            this.vert.Location = new System.Drawing.Point(3, 3);
             this.vert.Name = "vert";
-            this.vert.Size = new System.Drawing.Size(75, 23);
+            this.vert.Size = new System.Drawing.Size(25, 25);
             this.vert.TabIndex = 1;
-            this.vert.Text = "vert";
             this.vert.UseVisualStyleBackColor = false;
             this.vert.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
             // 
@@ -90,11 +89,10 @@
             this.jaune.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.jaune.Font = new System.Drawing.Font("Marlett", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.jaune.ForeColor = System.Drawing.SystemColors.Control;
-            this.jaune.Location = new System.Drawing.Point(165, 3);
+            this.jaune.Location = new System.Drawing.Point(127, 3);
             this.jaune.Name = "jaune";
-            this.jaune.Size = new System.Drawing.Size(75, 23);
+            this.jaune.Size = new System.Drawing.Size(25, 25);
             this.jaune.TabIndex = 2;
-            this.jaune.Text = "jaune";
             this.jaune.UseVisualStyleBackColor = false;
             this.jaune.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
             // 
@@ -104,11 +102,10 @@
             this.bleur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bleur.Font = new System.Drawing.Font("Marlett", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.bleur.ForeColor = System.Drawing.SystemColors.Control;
-            this.bleur.Location = new System.Drawing.Point(246, 3);
+            this.bleur.Location = new System.Drawing.Point(65, 3);
             this.bleur.Name = "bleur";
-            this.bleur.Size = new System.Drawing.Size(75, 23);
+            this.bleur.Size = new System.Drawing.Size(25, 25);
             this.bleur.TabIndex = 3;
-            this.bleur.Text = "bleu";
             this.bleur.UseVisualStyleBackColor = false;
             this.bleur.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
             // 
@@ -118,21 +115,20 @@
             this.gris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.gris.Font = new System.Drawing.Font("Marlett", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gris.ForeColor = System.Drawing.SystemColors.Control;
-            this.gris.Location = new System.Drawing.Point(327, 3);
+            this.gris.Location = new System.Drawing.Point(34, 3);
             this.gris.Name = "gris";
-            this.gris.Size = new System.Drawing.Size(75, 23);
+            this.gris.Size = new System.Drawing.Size(25, 25);
             this.gris.TabIndex = 4;
-            this.gris.Text = "gris";
             this.gris.UseVisualStyleBackColor = false;
             this.gris.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button4_MouseDown);
             // 
             // scrollertaille
             // 
             this.scrollertaille.LargeChange = 3;
-            this.scrollertaille.Location = new System.Drawing.Point(13, 45);
+            this.scrollertaille.Location = new System.Drawing.Point(697, 32);
             this.scrollertaille.Minimum = 1;
             this.scrollertaille.Name = "scrollertaille";
-            this.scrollertaille.Size = new System.Drawing.Size(165, 45);
+            this.scrollertaille.Size = new System.Drawing.Size(100, 45);
             this.scrollertaille.TabIndex = 1;
             this.scrollertaille.Value = 1;
             this.scrollertaille.Scroll += new System.EventHandler(this.Scroll);
@@ -143,11 +139,11 @@
             this.black.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.black.Font = new System.Drawing.Font("Marlett", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.black.ForeColor = System.Drawing.SystemColors.Control;
-            this.black.Location = new System.Drawing.Point(84, 3);
+            this.black.Location = new System.Drawing.Point(733, 3);
             this.black.Name = "black";
-            this.black.Size = new System.Drawing.Size(75, 23);
+            this.black.Size = new System.Drawing.Size(55, 23);
             this.black.TabIndex = 5;
-            this.black.Text = "noire";
+            this.black.Text = "starter";
             this.black.UseVisualStyleBackColor = false;
             this.black.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button5_MouseDown);
             // 
@@ -157,11 +153,11 @@
             this.white.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.white.Font = new System.Drawing.Font("Marlett", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.white.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.white.Location = new System.Drawing.Point(3, 3);
+            this.white.Location = new System.Drawing.Point(669, 3);
             this.white.Name = "white";
-            this.white.Size = new System.Drawing.Size(75, 23);
+            this.white.Size = new System.Drawing.Size(55, 23);
             this.white.TabIndex = 6;
-            this.white.Text = "blanc";
+            this.white.Text = "Gomme";
             this.white.UseVisualStyleBackColor = false;
             this.white.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button6_MouseDown);
             // 
@@ -169,8 +165,8 @@
             // 
             this.lookForColor.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.lookForColor.Font = new System.Drawing.Font("Marlett", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lookForColor.ForeColor = System.Drawing.SystemColors.Control;
-            this.lookForColor.Location = new System.Drawing.Point(480, 15);
+            this.lookForColor.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lookForColor.Location = new System.Drawing.Point(179, 6);
             this.lookForColor.Name = "lookForColor";
             this.lookForColor.Size = new System.Drawing.Size(75, 23);
             this.lookForColor.TabIndex = 8;
@@ -178,34 +174,37 @@
             this.lookForColor.UseVisualStyleBackColor = false;
             this.lookForColor.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button8_MouseDown);
             // 
-            // flowLayoutPanel2
+            // panel1
             // 
-            this.flowLayoutPanel2.Controls.Add(this.white);
-            this.flowLayoutPanel2.Controls.Add(this.black);
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(628, 12);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(163, 28);
-            this.flowLayoutPanel2.TabIndex = 5;
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Controls.Add(this.scrollertaille);
+            this.panel1.Controls.Add(this.black);
+            this.panel1.Controls.Add(this.lookForColor);
+            this.panel1.Controls.Add(this.white);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 84);
+            this.panel1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.scrollertaille);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.lookForColor);
-            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.draw_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.draw_MouseMove);
             this.flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.scrollertaille)).EndInit();
-            this.flowLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -221,7 +220,7 @@
         private System.Windows.Forms.Button black;
         private System.Windows.Forms.Button white;
         private System.Windows.Forms.Button lookForColor;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,9 +14,14 @@ namespace TP02
     {   private Point ptemps;
         private Color colortake;
         private int taille;
+
+
+        
         public Form1()
         {
             InitializeComponent();
+
+            
 
         }
 
@@ -71,13 +76,17 @@ namespace TP02
         private void button6_MouseDown(object sender, MouseEventArgs e)
         {
             this.colortake = Color.White;
+            
+            
         }
 
         private void draw_MouseMove(object sender, MouseEventArgs e)
         {
+            
             if (e.Button == MouseButtons.Left)
             {
                 Graphics g = this.CreateGraphics();
+
                 Pen p = new Pen(this.colortake, this.taille);
                 //           Rectangle r = new Rectangle(e.X, e.Y, 250, 130);
                 //           g.DrawRectangle(p, r);
@@ -86,13 +95,18 @@ namespace TP02
                 
                 g.DrawLine(p, pt1, pt2);
                 ptemps = new Point(e.X, e.Y);
+  
             }
+            
         }
 
         private void draw_MouseDown(object sender, MouseEventArgs e)
         {
             ptemps = new Point(e.X, e.Y);
+            
         }
+
+
 
     }
 }
